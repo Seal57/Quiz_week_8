9bc2cb101baa7074dd00d81daee799b4d5621a82
Seal57 <143397217+Seal57@users.noreply.github.com>
2023-09-16 00:47:20 +1000

    Add files via upload

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,29 @@
-import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
 
-plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+# Connect to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
 
-plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+# Define lists to store data
+Year = []
+CO2 = []
+Temperature = []
+
+
+# Fetch values from the database
+cursor.execute('SELECT Year, CO2, Temperature, FROM ClimateData')
+data = cursor.fetchall()
+
+# Populate lists with corresponding values
+for row in data:
+    Year.append(row[0])
+    CO2.append(row[1])
+    Temperature.append(row[2])
+
+# Close the connection
+conn.close()
+
+# Test print to verify data
+print("Year:", Year)
+print("CO2:", CO2)
+print("Temperature:", Temperature)
--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,30 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-years = []
-co2 = []
-temp = []
+
+data = pd.read_csv('climate.csv')
+
+years = data['Year']
+co2_levels = data['CO2']
+temperatures = data['Temperature']
+
+plt.figure(figsize=(10, 6))
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2_levels, color='red')
+plt.title('CO2 Levels')
+plt.xlabel('Year')
+plt.ylabel('CO2 Level (ppm)')
+
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temperatures, color='blue')
+plt.title('Temperature Data')
+plt.xlabel('Year')
+plt.ylabel('Temperature (°C)')
 
+
+plt.tight_layout()
+
+
+plt.show()
